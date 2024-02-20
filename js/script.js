05cb53744ce4145aab9a80126218466256a3fc03
LeeDaYoungss <leedayeonggg@gmail.com>
2024-02-21 00:58:12 +0900

    menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-let gnbWrap = document.querySelector(".menubar");
-let gnb = document.querySelectorAll(".menubar-wrapper");
+const mainMenu = document.querySelectorAll('.menu > li');
+const header = document.querySelector('.menubar_wrap');
+const initHeight = header.offsetHeight;
+let tallestHeight = 0;
 
+for(let mm of mainMenu){
+	//서브메뉴의 높이구하기
+	let smHeight = mm.querySelector('.sub_menu').offsetHeight;	
 
-for (let i = 0; i < gnb.length; i++) {
-    gnb[i].addEventListener("mouseover",() => {
-      gnbWrap.classList.add("on")
-    });
-};
+	if(smHeight > tallestHeight){
+		tallestHeight = smHeight;
+	}
+	let headerHeight = tallestHeight + initHeight + 50;
+	
+	mm.addEventListener('mouseover',()=>{
+		header.style.height = `${headerHeight}px`;
+	});
+	mm.addEventListener('mouseout',()=>{
+		header.style.height = `${initHeight}px`;
+	});	
+}
