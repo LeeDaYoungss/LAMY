657b5c076df71d8db192c6715d54d83dd5a1f01e
LeeDaYoungss <131832453+LeeDaYoungss@users.noreply.github.com>
2024-02-26 01:50:42 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,23 +4,21 @@ const initHeight = header.offsetHeight;
 let tallestHeight = 0;
 
 for(let mm of mainMenu){
-	//서브메뉴의 높이구하기
-	let smHeight = mm.querySelector('.sub_menu').offsetHeight;	
+  //서브메뉴의 높이구하기
+  let smHeight = mm.querySelector('.sub_menu').offsetHeight;	
 
-	if(smHeight > tallestHeight){
-		tallestHeight = smHeight;
-	}
-	let headerHeight = tallestHeight + initHeight + 50;
-	
-	mm.addEventListener('mouseover',()=>{
-		header.style.height = `${headerHeight}px`;
-	});
-	mm.addEventListener('mouseout',()=>{
-		header.style.height = `${initHeight}px`;
-	});	
+  if(smHeight > tallestHeight){
+    tallestHeight = smHeight;
+  }
+  let headerHeight = tallestHeight + initHeight + 50;
+  
+  mm.addEventListener('mouseover',()=>{
+    header.style.height = `${headerHeight}px`;
+  });
+  mm.addEventListener('mouseout',()=>{
+    header.style.height = `${initHeight}px`;
+  });	
 }
-
-
 
 /* MAIN BANNER */
 
